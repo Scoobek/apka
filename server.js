e4236359fcf7e45305d05f0e1bfa8fb7981e9846
scoobek <wojciech.skubisz@gmail.com>
2019-11-25 20:13:39 +0100

    Api post

--- server.js
+++ server.js
@@ -4,4 +4,6 @@ const port = process.env.PORT  || 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.post('/dupa', (req, res) => res.send('jesteś w dupie'))
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
