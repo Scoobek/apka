a70e43c68c89dc8308c1cb2dff3123355a4d9de8
scoobek <wojciech.skubisz@gmail.com>
2019-11-25 20:23:49 +0100

    Api post 3

--- server.js
+++ server.js
@@ -2,9 +2,26 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(express.json())
+
+const courses = [
+  { id: 1, name: "Algorithms" },
+  { id: 2, name: "Software Engineering" },
+  { id: 3, name: "Human Computer Interaction" }
+]
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.post('/dupa', (req, res) => res.send('jesteś w dupie'))
+app.post('/dupa', (req, res) => {
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name
+  };
+  //add the course to the array
+  courses.push(course);
+  //return the course
+  res.send(course);
+})
 
 app.post('/kaka', (req, res) => res.send('jetses kaka'))
 
